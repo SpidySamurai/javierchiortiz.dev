388631be2c6a40873b408a9cddfd42dd1d20dbda
SpidySamurai <javierchiortiz@gmail.com>
2025-11-18 18:51:29 -0600

    chore: apply max width header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,33 +5,32 @@ import Navbar from './Navbar';
 
 const Header = () => {
   return (
-  <header className="sticky top-0 z-40 w-full px-4 py-2 shadow-md bg-primary/95 flex items-center">
-      {/* Left: logo */}
-      <div className="flex items-center">
-        <Link href="/" className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="48"
-            height="48"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="text-white"
-          >
-            <path d="M17 3v10a4 4 0 0 1-8 0" />
-          </svg>
-        </Link>
-      </div>
+    <header className="sticky top-0 z-40 w-full px-4 py-2 shadow-md bg-primary/95">
+      <div className="w-full max-w-screen-xl mx-auto flex items-center justify-between">
+        {/* Left: logo */}
+        <div className="flex items-center">
+          <Link href="/" className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="48"
+              height="48"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="text-white"
+            >
+              <path d="M17 3v10a4 4 0 0 1-8 0" />
+            </svg>
+          </Link>
+        </div>
 
-      {/* Middle spacer */}
-      <div className="flex-1" />
-
-      {/* Right: navbar */}
-      <div className="flex items-center justify-end">
-        <Navbar />
+        {/* Right: navbar */}
+        <div className="flex items-center justify-end">
+          <Navbar />
+        </div>
       </div>
     </header>
   );
